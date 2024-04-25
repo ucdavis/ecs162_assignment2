3bd1358605e91f1e2d763790704b2ce04c4e0031
Mihika Krishna <krishna.mihika@gmail.com>
2024-04-25 16:08:00 -0700

    Add basic code to move snake

--- js/snake.js
+++ js/snake.js
@@ -1,9 +1,16 @@
 const gridContainer = document.getElementById("grid-container");
 const gridDimension = 20;
+const directions = [
+    [0, 1], // down
+    [1, 0], // right
+    [0, -1], // up
+    [-1, 0] // left
+]
 
 // array of tuples representing each segment of the snake
 let snake_coordinates = [[10, 10]];
 let apple_coordinates = [[0, 0]];
+let snake_direction = directions[0];
 
 function createGrid() {
 	for (let i = 0; i < gridDimension * gridDimension; i++) {
@@ -32,6 +39,7 @@ function clearCells() {
 }
 
 function drawSnake(){
+    moveSnake();
     fillCells(snake_coordinates, 'green');
 }
 
@@ -47,7 +55,29 @@ function drawApple(){
     fillCells(apple_coordinates, 'red');
 }
 
-// make snake continously move
+function moveSnake(){
+    snake_coordinates.forEach((segment) => {
+        segment[0] += snake_direction[0];
+        segment[1] += snake_direction[1];
+    });
+}
+
+document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 37:
+            snake_direction = directions[3];
+            break;
+        case 38:
+            snake_direction = directions[2];
+            break;
+        case 39:
+            snake_direction = directions[1];
+            break;
+        case 40:
+            snake_direction = directions[0];
+            break;
+    }
+};
 
 // if snake bumps into wall, game over
 
