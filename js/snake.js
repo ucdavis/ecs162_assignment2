f8a9f08a4801b05275a506b23e958046c67f5bd3
Mihika Krishna <krishna.mihika@gmail.com>
2024-04-25 20:50:12 -0700

    Add difficulty buttons and styling

--- js/snake.js
+++ js/snake.js
@@ -122,6 +122,14 @@ document.onkeydown = function (e) {
 	}
 };
 
+function toggleButtonShadow(button) {
+    document.querySelectorAll('.button-container button').forEach(btn => {
+        btn.classList.remove('invert-shadow');
+    });
+
+    button.classList.add('invert-shadow');
+}
+
 createGrid();
 
 function gameLoop() {
